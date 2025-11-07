48f833638559cc0cdaf37449fce71364bd0a3a67
darkzone44 <123981910+darkzone44@users.noreply.github.com>
2025-11-07 16:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def send_e2ee_message(fb_user, fb_pass, chat_id, msg, delay, console):
         if not input_box:
             logs.append("[ERROR] Message input not found! FB layout update ho sakta hai.")
             driver.quit()
+            # FIX: logs join
             console.markdown('<div class="console">' + "
 ".join(logs) + '</div>', unsafe_allow_html=True)
             return
@@ -94,6 +95,7 @@ def send_e2ee_message(fb_user, fb_pass, chat_id, msg, delay, console):
         logs.append(f"[ERROR] {str(e)}")
         driver.quit()
 
+    # FIX: logs join
     console.markdown('<div class="console">' + "
 ".join(logs) + '</div>', unsafe_allow_html=True)
 
